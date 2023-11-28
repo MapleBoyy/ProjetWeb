68743f7b66c615ee3500a015cf63f1bf31095cd6
Romain Liebert <87571366+Romainlieb@users.noreply.github.com>
2023-11-28 15:58:12 +0100

    La galère, j'ai commencé, essayé, pas réussi mais on va réussir

--- Express.js
+++ Express.js
@@ -1,11 +1,59 @@
-const express = require('express');
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { engine } from 'express-handlebars';
+
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+
+// User routes
+app.post('/register', async (req, res) => {
+  try {
+    // Extract the user data from the request body
+    const { username, email, password } = req.body;
+
+    // Perform validation on the user data (e.g., check for required fields, validate email format, etc.)
+
+    // Create a new user in the database using the Prisma client
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password,
+      },
+    });
+
+    // Return a success response with the newly created user
+    res.status(201).json(newUser);
+  } catch (error) {
+    // Handle any errors that occur during the registration process
+    console.error('Error during registration:', error);
+    res.status(500).json({ error: 'An error occurred during registration' });
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.post('/login', async (req, res) => {
+  // Login logic
 });
 
+// Group routes
+app.post('/group', async (req, res) => {
+  // Group creation logic
+});
+
+app.post('/group/:id/invite', async (req, res) => {
+  // Invitation logic
+});
+
+// Reminder routes
+app.post('/reminder', async (req, res) => {
+  // Reminder creation logic
+});
+
+app.listen(3010, () => {
+  console.log('Server is running on port 3010');
+});
