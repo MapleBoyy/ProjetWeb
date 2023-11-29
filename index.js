12636b9a01e35497ce062a9b3bbde9c3926d1390
Romain Liebert <87571366+Romainlieb@users.noreply.github.com>
2023-11-29 12:10:03 +0100

    Index.js inutile le laisser sous commentaire

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
+// import express from 'express';
+// import { engine } from 'express-handlebars';
 
-const app = express();
-const port = 3010;
+// const app = express();
+// const port = 3010;
 
-const userRouter = express.Router();
+// const userRouter = express.Router();
 
-const logger = (req, _res, next) => {
-  console.log(`IP: ${req.ip}, Method: ${req.method}, Route: ${req.originalUrl}, Date: ${new Date().toLocaleString()}`);
-  next();
-};
+// const logger = (req, _res, next) => {
+//   console.log(`IP: ${req.ip}, Method: ${req.method}, Route: ${req.originalUrl}, Date: ${new Date().toLocaleString()}`);
+//   next();
+// };
 
-// Apply the logger middleware
-app.use(logger);
+// // Apply the logger middleware
+// app.use(logger);
 
-// Configure Handlebars
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+// // Configure Handlebars
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.set('views', './views');
 
-// User routes
-userRouter.get('/register', (_req, res) => {
-  res.render('register');
-});
+// // User routes
+// userRouter.get('/register', (_req, res) => {
+//   res.render('register');
+// });
 
-userRouter.post('/register', async (_req, _res) => {
-  // Handle registration
-});
 
-// Apply the user router
-app.use('/user', userRouter);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
